13e0ef6b4d83ab9eaf6f90f62696de06763ca5b8
Rem Pimean <84150367+pimean@users.noreply.github.com>
2022-03-21 02:17:46 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+#
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVeHD8NemS6fa8dojuQWgyBoqpcbauQCx6.workename -p x --cpu 4
